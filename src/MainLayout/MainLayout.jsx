2bd2bd7c7f48e7c7e790af4d83baa9ad83ec78a3
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-28 20:39:56 +0600

    navbar

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Footer from '../pages/Shared/Footer/Footer';
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../pages/Shared/Footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="min-h-[calc(100vh-523.89px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default MainLayout;
