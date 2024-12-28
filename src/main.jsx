8f487f1ed3a9b9576ece0f8006f5388a00353f05
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-28 20:23:05 +0600

    footer design done

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from './routes/router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <div className="max-w-screen-xl mx-auto">
+      <RouterProvider router={router} />
+    </div>
+  </StrictMode>
+);
--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React from "react";
+import { FaFacebook, FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <footer>
+      <div className="grid grid-cols-1 sm:grid-cols-2 text-neutral-content bg-[#111827]">
+        <aside className="bg-[#1F2937] p-6 sm:p-10 md:p-24 flex flex-col justify-center items-center text-center">
+          <h2 className="text-3xl uppercase mb-6">Contact Us</h2>
+          <div className="text-xl space-y-3">
+            <p>123 ABS Street, Uni 21, Bangladesh</p>
+            <p>+88 123456789</p>
+            <p>Mon - Fri: 08:00 - 22:00</p>
+            <p>Sat - Sun: 10:00 - 23:00</p>
+          </div>
+        </aside>
+        <aside className="text-center flex flex-col justify-center items-center p-6 sm:p-10 md:p-24">
+          <h2 className="text-3xl">Follow Us</h2>
+          <p className="text-xl">Join us on social media</p>
+          <div className="flex justify-center items-center text-xl mt-3 gap-3">
+            <FaFacebook className="cursor-pointer" />
+            <FaInstagram className="cursor-pointer" />
+            <FaXTwitter className="cursor-pointer" />
+          </div>
+        </aside>
+      </div>
+      <div className="footer-center bg-base-300 text-base-content p-4">
+        <aside>
+          <p>
+            Copyright © {new Date().getFullYear()} - CulinaryCloud. All rights
+            reserved.
+          </p>
+        </aside>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
