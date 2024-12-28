59d53bc3b0cf479a24a234d8a97ef985e3bc5d1c
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-28 19:54:21 +0600

    project setup

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h2 className="text-4xl">Homepage</h2>
+        </div>
+    );
+};
+
+export default HomePage;
