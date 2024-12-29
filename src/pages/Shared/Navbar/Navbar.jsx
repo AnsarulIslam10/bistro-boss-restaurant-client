e6ad25daea9bb237d2626254498dfa503f685b9b
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-29 12:31:15 +0600

    navbar update

--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,30 +1,66 @@
 import React from "react";
+import { FaUserCircle } from "react-icons/fa";
 import { NavLink } from "react-router-dom";
-
+import cartImg from "../../../assets/icon/151-1511569_cart-notifications-free-shopping-cart-favicon-hd-png-removebg-preview.png"
 const Navbar = () => {
   const links = (
     <>
       <li>
-        <NavLink to={"/"}>HOME</NavLink>
+        <NavLink
+          to={"/"}
+          className={({ isActive }) =>
+            `font-bold ${isActive ? "text-[#EEFF25]" : ""}`
+          }
+        >
+          HOME
+        </NavLink>
       </li>
       <li>
-        <NavLink to={"/contact-us"}>CONTACT US</NavLink>
+        <NavLink
+          to={"/contact-us"}
+          className={({ isActive }) =>
+            `font-bold ${isActive ? "text-[#EEFF25]" : ""}`
+          }
+        >
+          CONTACT US
+        </NavLink>
       </li>
       <li>
-        <NavLink to={"/dashboard"}>DASHBOARD</NavLink>
+        <NavLink
+          to={"/dashboard"}
+          className={({ isActive }) =>
+            `font-bold ${isActive ? "text-[#EEFF25]" : ""}`
+          }
+        >
+          DASHBOARD
+        </NavLink>
       </li>
       <li>
-        <NavLink to={"/our-menu"}>OUR MENU</NavLink>
+        <NavLink
+          to={"/our-menu"}
+          className={({ isActive }) =>
+            `font-bold ${isActive ? "text-[#EEFF25]" : ""}`
+          }
+        >
+          OUR MENU
+        </NavLink>
       </li>
       <li>
-        <NavLink to={"/our-shop"}>OUR SHOP</NavLink>
+        <NavLink
+          to={"/our-shop"}
+          className={({ isActive }) =>
+            `font-bold ${isActive ? "text-[#EEFF25]" : ""}`
+          }
+        >
+          OUR SHOP
+        </NavLink>
       </li>
     </>
   );
   return (
     <>
-      <div className="navbar fixed z-10 bg-black bg-opacity-30 py-4 md:pt-8 md:pb-6 text-white max-w-screen-xl mx-auto">
-        <div className="navbar-start">
+      <div className="navbar fixed z-10 bg-black bg-opacity-30 py-4 md:pt-8 md:pb-6 text-white justify-between max-w-screen-xl mx-auto">
+        <div className="">
           <div className="dropdown">
             <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
               <svg
@@ -44,18 +80,27 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-black/50 rounded-none z-[1] mt-3 w-52 p-2 shadow"
             >
               {links}
             </ul>
           </div>
-          <a className="btn btn-ghost text-xl">BISTRO BOSS</a>
+          <a className="text-xl">
+          <p className="uppercase text-3xl font-bold">Bestro Boss</p>
+          <p className="uppercase text-2xl tracking-widest">Restaurant</p>
+          </a>
+          
         </div>
-        <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">{links}</ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn">Sign in</a>
+
+        <div className="">
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal px-1">{links}</ul>
+          </div>
+          <a href="">
+            <img src={cartImg} className="w-12 mr-5" alt="" />
+          </a>
+          <a className=" flex items-center font-bold uppercase gap-1">Sign Out <FaUserCircle className="text-3xl" /></a>
+          
         </div>
       </div>
     </>
