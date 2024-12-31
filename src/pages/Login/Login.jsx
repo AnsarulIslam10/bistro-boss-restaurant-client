0a542c014274a9b41c43676a55c65b3e9d50ba4a
AnsarulIslam10 <ansarulislam01739@gmail.com>
2024-12-31 21:17:19 +0600

    captcha added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,8 +1,39 @@
 import { Link } from "react-router-dom";
 import bgImg from "../../assets/others/authentication.png";
 import loginImg from "../../assets/others/authentication2.png";
-import { FaFacebook, FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import { FaFacebookF, FaGithub, FaGoogle } from "react-icons/fa";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  validateCaptcha,
+} from "react-simple-captcha";
+import { useEffect, useRef, useState } from "react";
+
 const Login = () => {
+  const captchaRef = useRef();
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log({ email, password });
+  };
+
+  const handleValidateCaptch = () => {
+    const user_captcha_value = captchaRef.current.value;
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
+  };
+
   return (
     <div
       className="hero min-h-screen text-[#444444] absolute left-0 px-40"
@@ -18,8 +49,10 @@ const Login = () => {
           <img src={loginImg} alt="" />
         </div>
         <div className="card w-full max-w-sm shrink-0 ">
-          <form className="card-body">
-            <h2 className="text-center text-[40px] font-bold mb-5 text-[#151515]">Login</h2>
+          <form onSubmit={handleLogin} className="card-body">
+            <h2 className="text-center text-[40px] font-bold mb-5 text-[#151515]">
+              Login
+            </h2>
             <div className="form-control">
               <label className="label">
                 <span className="text-lg font-semibold">Email</span>
@@ -43,14 +76,30 @@ const Login = () => {
                 className="input input-bordered"
                 required
               />
-              {/* <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
-              </label> */}
+            </div>
+            <div className="form-control">
+              <LoadCanvasTemplate />
+              <input
+                ref={captchaRef}
+                type="text"
+                placeholder="Type here"
+                name="captcha"
+                className="input rounded-b-none input-bordered focus:outline-none"
+                required
+                onBlur={handleValidateCaptch}
+              />
+              {/* <button
+                onClick={handleValidateCaptch}
+                className="btn btn-outline border-none hover:bg-[rgba(184,140,74,0.44)] text-black bg-[rgba(209,159,84,0.52)] btn-xs rounded-t-none"
+              >
+                Verify
+              </button> */}
             </div>
             <div className="form-control mt-6">
-              <button className="btn rounded-lg text-white font-bold bg-[rgba(209,160,84,0.7)] mb-2">
+              <button
+                disabled={disabled}
+                className="btn rounded-lg text-white font-bold bg-[rgba(209,160,84,0.7)] mb-2"
+              >
                 Login
               </button>
             </div>
