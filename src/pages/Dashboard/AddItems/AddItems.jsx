179ea630a16bb7e597fb5bd84c93823360f5224f
AnsarulIslam10 <ansarulislam01739@gmail.com>
2025-01-05 20:46:21 +0600

    add item to database

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -2,11 +2,49 @@ import React from "react";
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from "react-hook-form";
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
+
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
 
 const AddItems = () => {
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data) => {
+  const { register, handleSubmit, reset } = useForm();
+  const axiosPublic = useAxiosPublic()
+  const axiosSecure = useAxiosSecure()
+  const onSubmit = async (data) => {
     console.log(data);
+    //image upload to imgbb then get an url
+    const imageFile = {image: data.image[0]}
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    })
+    if (res.data.success) {
+        //now send the menu item data to server with the image
+        const menuItem ={
+            name: data.name,
+            categoty: data.categoty,
+            price: parseFloat(data.price),
+            recipe: data.recipe,
+            image: res.data.data.display_url,
+        }
+        const menuRes = await axiosSecure.post('/menu', menuItem)
+        console.log(menuRes.data)
+        if (menuRes.data.insertedId) {
+            reset()
+            Swal.fire({
+                title: `${data.name} is added to the menu`,
+                icon: "success",
+                showCancelButton: false,
+                timer: 1500
+            })
+        }
+
+    }
+    // console.log(res.data)
   };
   return (
     <div>
